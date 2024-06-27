08bc41f767d2378dfe93ff7ebf8f288498de261e
IT22079572 <it22079572@my.sliit.lk>
2024-06-27 16:41:26 +0530

    initial commit on JS

---
+++ demo.js
@@ -0,0 +1,185 @@
+/*
+function abc()
+{
+    console.log("The Data uploaded Successfully !")
+}
+
+function abcd()
+{
+    console.log("The server running Successfully !")
+}
+
+
+abc()
+abcd()
+
+*/
+
+/*
+var fActor = "Kamal"
+var fPlayer = "Dhoni"
+var fMovie = "Dark Knight"
+
+function Fav()
+{
+    console.log("favourite Actor : " + fActor)
+    console.log("favourite Player : " + fPlayer)
+    console.log("favourite Movie : " + fMovie)
+}
+
+Fav()
+
+
+
+var jobDone = false
+
+if(jobDone) {
+    console.log("Work Done Successfully")
+
+}
+else {
+    console.log("Work Undone !!")
+}
+
+
+
+for(c = 1; c <=10; c = c+1)
+{
+    console.log("Azaam Ahmed")
+}
+
+for(c = 1; c <=10; c = c+1)
+{
+    console.log(c)
+}
+
+for(c = 1; c <= 20; c = c+2)
+{
+    console.log(c)
+}
+
+
+
+var box1=document.getElementById("num1")
+    var box2=document.getElementById("num2")
+
+    var results = document.getElementById("results")
+
+    function Addfunction()
+    {
+        var box1value = Number(box1.value)
+        var box2value = Number(box2.value)
+
+        var total = box1value + box2value
+
+        results.textContent = total
+    }
+    
+    function Subfunction()
+    {
+        var box1value = Number(box1.value)
+        var box2value = Number(box2.value)
+
+        var total = box1value - box2value
+
+        results.textContent = total
+    }
+
+    function Mulfunction()
+    {
+        var box1value = Number(box1.value)
+        var box2value = Number(box2.value)
+
+        var total = box1value * box2value
+
+        results.textContent = total
+    }
+
+    function Divfunction()
+    {
+        var box1value = Number(box1.value)
+        var box2value = Number(box2.value)
+
+        var total = box1value / box2value
+
+        results.textContent = total
+    }
+
+
+*/
+
+/*
+let eng = 95, tamil = 96, maths = 95;
+let total,avg;
+
+total = eng + tamil + maths;
+avg = total / 3;
+
+console.log("Total   : "+total)
+console.log("Average : "+avg.toFixed(2))
+
+if (eng >=35 && tamil >=35 && maths >=35)
+    {
+        console.log("Results : Pass")
+
+        if(avg >90 && avg <=100)
+            {
+                console.log("Grade   : A grade")
+            }
+        else if (avg >80 && avg <=90)
+            {
+                console.log("Grade   : B grade")
+            }
+        else if (avg >70 && avg <=80)
+            {
+                console.log("Grade   : C grade")
+            }
+        else 
+            {
+                console.log("Grade   : D grade")
+            }
+    }
+    else
+    {
+        console.log("Results : Fail")
+        console.log("Grade   : No Grade")
+    }
+
+
+
+    let arr=[]
+for(let i=0;i<=100;i+=2)
+    {
+        arr.push(i)
+    }
+console.log(arr)
+console.log(arr[20])
+
+
+ */
+// project 01 in Udemy
+
+/*
+
+console.log("Helloo ! Welcome to Food App");
+console.log("Lets create an account");
+
+var uname = prompt("Please create a username    : ");
+var password = prompt("Please create a password : ");
+
+console.log("Please ! Enter your Login Credential !");
+var typeuser = prompt("User Name  : ");
+var typepas  = prompt("Password   : ");
+
+while(uname!=typeuser && password != typepas)
+    {
+        console.log("Wrong User Credentials!");
+        var typeuser = prompt("User Name  : ");
+        var typepas  = prompt("Password   : ");
+    }
+
+console.log("Ypu are Connected.")
+
+*/
+
+
